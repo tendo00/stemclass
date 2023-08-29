9d0b7e887e8dc1332f009270ea0a03fe4fa0b538
tendo00 <helainetutu88@gmail.com>
2023-08-29 16:27:56 +0300

    firstwork

---
+++ main.py
@@ -0,0 +1,16 @@
+name='hellen'
+age = 25
+gender ='F'
+marks = 56.8
+
+print(name)
+print(age)
+print(gender)
+print(marks)
+
+student=['hellen',25,'F',12]
+
+print(student[2])
+print(student[0])
+print(student[1])
+print(student[3])
